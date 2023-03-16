c414b1e50dd0f67912a0a79f3cd9d8fa95e3fbdf
Hegedus-Gergo <71663811+Hegedus-Gergo@users.noreply.github.com>
2023-03-16 21:41:20 +0100

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,43 +16,99 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp6
+namespace WpfOsztalyzas
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        bool vanErtekMegadva = false;
-        String nev;
-        String datum;
-        String tantargy;
-        int jegy;
+        string fajlNev = "naplo.txt";
+        ObservableCollection<Osztalyzat> jegyek = new ObservableCollection<Osztalyzat>();
+
         public MainWindow()
         {
             InitializeComponent();
-            dpDatum.SelectedDate = DateTime.Today;
-            lblJegy.Content = sliJegy.Value;
+            var dialog = new OpenFileDialog();
+            dialog.FileName = "naplo.csv";
+            dialog.DefaultExt = ".csv"; 
+            dialog.Filter = "Text documents (.csv)|*.csv";
+            bool? result = dialog.ShowDialog();
+            jegyek.Clear();
+            StreamReader sr = new StreamReader(fajlNev);
+            while (!sr.EndOfStream)
+            {
+                string[] mezok = sr.ReadLine().Split(";");
+                Osztalyzat ujJegy = new Osztalyzat(mezok[0], mezok[1], mezok[2], int.Parse(mezok[3]));
+                jegyek.Add(ujJegy);
+            }
+            sr.Close();
+            dgJegyek.ItemsSource = jegyek;
+            lblUtvonal.Content = dialog.FileName;
+            lblJegyekSzamaSzam.Content = dgJegyek.Items.Count;
+            datDatum.SelectedDate = DateTime.Now;
         }
 
-        private void cboTantargy_Selected(object sender, RoutedEventArgs e)
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
         {
+            string teljesNev = txtNev.Text;
+            string[] nevSplit = teljesNev.Split(" ");
+            int nevekSzama = nevSplit.Count();
+            bool nevKevesebbMintHaromKarakter = false;
+            DateTime currentDateTime = DateTime.Now;
+            DateTime selectedDateTime = datDatum.SelectedDate.Value;
+            foreach (string nevResz in nevSplit)
+            {
+                if (nevResz.Count() < 3)
+                {
+                    nevKevesebbMintHaromKarakter = true;
+                    break;
+                }
+            }
 
+            if (nevekSzama >= 2)
+            {
+                if (nevKevesebbMintHaromKarakter == false)
+                {
+                    if (selectedDateTime < currentDateTime)
+                    {
+                        string csvSor = $"{txtNev.Text};{datDatum.Text};{cboTantargy.Text};{sliJegy.Value}";
+                        StreamWriter sw = new StreamWriter(fajlNev, append: true);
+                        sw.WriteLine(csvSor);
+                        sw.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nem lehet jövőbeli időpontot beállítani!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("A nevek legalább 3 betűből kell álljanak!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ez nem egy teljes név!");
+            }
+            dgJegyek.ItemsSource = jegyek;
+            dgJegyek.Items.Refresh();
         }
 
-        private void dpDatum_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        private void btnBetolt_Click(object sender, RoutedEventArgs e)
         {
-
+            jegyek.Clear();
+            StreamReader sr = new StreamReader(fajlNev);
+            while (!sr.EndOfStream)
+            {
+                string[] mezok = sr.ReadLine().Split(";");
+                Osztalyzat ujJegy = new Osztalyzat(mezok[0], mezok[1], mezok[2], int.Parse(mezok[3]));
+                jegyek.Add(ujJegy);
+            }
+            sr.Close();
+            dgJegyek.ItemsSource = jegyek;
         }
 
         private void sliJegy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            lblJegy.Content = Math.Round(sliJegy.Value);
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            vanErtekMegadva = true;
+            lblJegy.Content = sliJegy.Value; 
         }
     }
 }
